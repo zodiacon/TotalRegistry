b40b822a53bc9dbd11416e64264af1df39f80279
Pavel Yosifovich <zodiacon@live.com>
2022-09-14 19:41:26 -0400

    fixed regression: Ctrl+Z and Ctrl+Y not working

--- RegExp/resource.h
+++ RegExp/resource.h
@@ -202,7 +202,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        182
-#define _APS_NEXT_COMMAND_VALUE         32884
+#define _APS_NEXT_COMMAND_VALUE         32887
 #define _APS_NEXT_CONTROL_VALUE         1060
 #define _APS_NEXT_SYMED_VALUE           115
 #endif
